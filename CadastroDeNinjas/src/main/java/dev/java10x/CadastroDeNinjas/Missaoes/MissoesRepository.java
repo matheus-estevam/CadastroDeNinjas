8406ff5446e6077005b27e8926915a8fbbc2e978
matheus-estevam <matheusestevam17055@gmail.com>
2025-08-25 13:25:11 -0300

    feat: Coloca os códigos num padrap da arquitetura layer

---
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Missaoes/MissoesRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Missaoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
